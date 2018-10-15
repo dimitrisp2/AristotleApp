211f24c6b40933b0eb06993739ed847e72c22ad4
Dimitris Papadimitriou <info@dimitrisp.eu>
2018-10-15 09:24:31 +0300

    Add link to user list Fix some capitalisations New logo

--- common/head.php
+++ common/head.php
@@ -18,7 +18,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="index.php">
-            <img src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt=""> Aristotle
+            <img src="assets/logo.png" width="30" height="30" class="d-inline-block align-top" alt=""> Aristotle
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -28,6 +28,7 @@
                 <a class="nav-item nav-link active" href="index.php">Home</a>
                 <a class="nav-item nav-link" href="projects.php">Projects</a>
                 <a class="nav-item nav-link" href="tasks.php">Tasks</a>
+                <a class="nav-item nav-link" href="users.php">Users</a>
             </div>
         </div>
     </nav>
